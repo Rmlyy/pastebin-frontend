ea979c461c94e12e265496800f1b9d74184a81d1
Rmlyy <hello@rmly.dev>
2023-08-10 13:33:37 +0300

    change api base url

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:9102'
+const API_BASE_URL = 'https://paste-api.rmly.dev'
 const MAX_PASTE_LENGTH = 100000
 
 const errorElem = document.getElementById('error')
